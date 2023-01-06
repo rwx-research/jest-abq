ecf1edb57561cde0a7673db028aff0e697b816c8
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2023-01-06 11:45:18 -0600

    Give facebook affiliation

--- e2e/abq/__tests__/equals.test.js
+++ e2e/abq/__tests__/equals.test.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 test('2 equals 1 + 1', () => {
   expect(2 === 1 + 1).toBe(true);
 });
--- e2e/abq/__tests__/error_setup.test.js
+++ e2e/abq/__tests__/error_setup.test.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 throw new Error("can't catch me");
 
 // eslint-disable-next-line no-unreachable
