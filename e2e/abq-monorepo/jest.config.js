e57ad7f36c9c3a0edcef06c18085a89c38ed3b34
Ayaz Hafiz <ayaz@rwx.com>
2023-06-07 11:42:01 -0500

    Copy

--- e2e/abq-monorepo/jest.config.js
+++ e2e/abq-monorepo/jest.config.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) ReadWriteExecute, Inc. and its affiliates. All Rights Reserved.
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 module.exports = {
   projects: ['<rootDir>/packages/*'],
   testEnvironment: 'node',
