1aac641e7dc2c0e440f75dc00664cbeef8701001
Ayaz Hafiz <ayaz@rwx.com>
2023-07-21 13:08:43 -0500

    Add copyright header

--- packages/jest-circus/src/eventsUtils.ts
+++ packages/jest-circus/src/eventsUtils.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) ReadWriteExecute, Inc. and its affiliates. All Rights Reserved.
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import {jestExpect} from '@jest/expect';
 import type {Circus} from '@jest/types';
 
